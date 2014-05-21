b15f2eee1b86bb967e4f19a74dda0704218a9dda
Matthew Shaw <matthew.shaw@landregistry.gsi.gov.uk>
2014-05-21 21:36:46 +0100

    Page Layouts

--- js/map.js
+++ js/map.js
@@ -1,12 +1,46 @@
-var map = new ol.Map({
-  target: 'map',
-  layers: [
-  new ol.layer.Tile({
-    source: new ol.source.OSM()
-  })
-  ],
-  view: new ol.View2D({
-    center: ol.proj.transform([-3.5, 55], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 6
-  })
+var layer = new ol.layer.Tile({
+  source: new ol.source.OSM()
 });
+layer.once('precompose', checkWebGL);
+
+var view = new ol.View2D({
+  center: [0, 0],
+  zoom: 2
+});
+
+var map = new ol.Map({
+  layers: [layer],
+  renderer: exampleNS.getRendererFromQueryString(),
+  target: 'map',
+  view: view
+});
+
+var visible = new ol.dom.Input(document.getElementById('visible'));
+visible.bindTo('checked', layer, 'visible');
+
+var opacity = new ol.dom.Input(document.getElementById('opacity'));
+opacity.bindTo('value', layer, 'opacity')
+    .transform(parseFloat, String);
+
+var hue = new ol.dom.Input(document.getElementById('hue'));
+hue.bindTo('value', layer, 'hue')
+    .transform(parseFloat, String);
+
+var saturation = new ol.dom.Input(document.getElementById('saturation'));
+saturation.bindTo('value', layer, 'saturation')
+    .transform(parseFloat, String);
+
+var contrast = new ol.dom.Input(document.getElementById('contrast'));
+contrast.bindTo('value', layer, 'contrast')
+    .transform(parseFloat, String);
+
+var brightness = new ol.dom.Input(document.getElementById('brightness'));
+brightness.bindTo('value', layer, 'brightness')
+    .transform(parseFloat, String);
+
+
+var rotation = new ol.dom.Input(document.getElementById('rotation'));
+rotation.bindTo('value', view, 'rotation').transform(parseFloat, String);
+
+var resolution = new ol.dom.Input(document.getElementById('resolution'));
+resolution.bindTo('value', view, 'resolution').transform(parseFloat, String);
